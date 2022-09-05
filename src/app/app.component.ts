8c36550a524c3068f1b6e90bdecedc1a9cdcd594
Serge Zoto <serginho-10101@live.fr>
2022-09-05 16:09:26 +0000

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  // Titre de la page
   title = 'todo-list';
+
+  tasks = [
+    "Visit Ann",
+    "Call dad",
+    "Go to gym",
+    "Wash the dishes",
+    "Shop of the party"
+  ]
+  // Delaration d'une method pour ajouter un todo
+  add(newtask: string){
+    this.tasks.push(newtask)
+  }
+
+
+  // Delaration d'une method pour supprimer un todo
+  remove(existingTask: string){
+    var userConfirmed = confirm(`Are you sure to remove this task \n This task ? "\n ${existingTask}" `)
+
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
 }
